3435aa06005be2fa1726e227f0647ff0b1aee766
Skye <aidantpurchase@gmail.com>
2021-03-27 15:28:03 +0000

    Initial commit

---
+++ DataAnalyser.py
@@ -0,0 +1,121 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from pprint import pprint
+
+import matplotlib.pyplot as plt
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+class DataAnalyser:
+    def __init__(self):
+        """Shows basic usage of the Google Calendar API.
+        Prints the start and name of the next 10 events on the user's calendar.
+        """
+        self.credentials = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                self.credentials = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not self.credentials or not self.credentials.valid:
+            if self.credentials and self.credentials.expired and self.credentials.refresh_token:
+                self.credentials.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                self.credentials = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(self.credentials, token)
+
+        self.service = build('calendar', 'v3', credentials=self.credentials)
+
+        self.calendar_info = {'Generic': 'primary',
+                              'Lecture Work': 'c_krn5r1k7ppkaoatkonrnaup63o@group.calendar.google.com',
+                              'Leisure': 'c_46544nas4bq1j0iubthl8m5bi8@group.calendar.google.com',
+                              'Menial Tasks': 'c_8sa66nob86sbnni235f5dfk1hg@group.calendar.google.com',
+                              'Supervisions': 'c_81fnb8n9bf3hau131nb02p35gc@group.calendar.google.com',
+                              'Lectures': 'a77nfv4okg843lato7vm58n3lnv3skan@import.calendar.google.com'}
+
+    def getDayDate(self, start: datetime):
+
+        end = start + datetime.timedelta(1)
+        raw_data = {}
+
+        for calendar, ID in self.calendar_info.items():
+            events_result = self.service.events().list(calendarId=ID,
+                                                       timeMin=start.isoformat() + 'Z',
+                                                       timeMax=end.isoformat() + 'Z',
+                                                       singleEvents=True,
+                                                       orderBy='startTime').execute()
+            raw_data[calendar] = events_result.get('items', [])
+
+        useful_data = {}
+
+        for calendar in raw_data.keys():
+            for event in raw_data[calendar]:
+                if 'dateTime' in event.get('start').keys():
+                    curr = datetime.datetime.strptime(event.get('start').get('dateTime'), '%Y-%m-%dT%H:%M:%SZ')
+                    end = datetime.datetime.strptime(event.get('end').get('dateTime'), '%Y-%m-%dT%H:%M:%SZ')
+
+                    while curr < end:
+                        new_event = {}
+                        key = str(curr)[11:19]
+                        new_event['calendar'] = calendar
+                        new_event['weekday'] = curr.weekday()
+
+                        useful_data[key] = new_event
+
+                        curr += datetime.timedelta(minutes=5)
+
+        return useful_data
+
+    def getCalendarData(self, calendar, start_date, days):
+        ID = self.calendar_info[calendar]
+        raw_data = {}
+
+        for day in range(days):
+            start = start_date + datetime.timedelta(day)
+            end = start + datetime.timedelta(1)
+
+            events_result = self.service.events().list(calendarId=ID,
+                                                       timeMin=start.isoformat() + 'Z',
+                                                       timeMax=end.isoformat() + 'Z',
+                                                       singleEvents=True,
+                                                       orderBy='startTime').execute()
+
+            raw_data[day + 1] = events_result.get('items', [])
+
+        useful_data = {}
+
+        for day in raw_data.keys():
+            for event in raw_data.get(day):
+                if 'dateTime' in event.get('start').keys():
+                    curr = datetime.datetime.strptime(event.get('start').get('dateTime'), '%Y-%m-%dT%H:%M:%SZ')
+                    end = datetime.datetime.strptime(event.get('end').get('dateTime'), '%Y-%m-%dT%H:%M:%SZ')
+
+                    while curr < end:
+                        new_event = {}
+                        key = str(curr)[11:19]
+
+                        if key in useful_data.keys():
+                            useful_data[key] += 1
+                        else:
+                            useful_data[key] = 1
+
+                        curr += datetime.timedelta(minutes=5)
+
+        for time in useful_data.keys():
+            useful_data[time] /= days
+
+        return useful_data
